a8d1f9a591c95036cee4061bf692fec071e5f4a4
watanabehideki <hideki.art.2280@gmail.com>
2021-01-10 23:21:14 +0900

    Gallery.js作成

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
 
 function Gallery() {
+
+  const [gallery, setGallery] = useState({})
+  let { id } = useParams()
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `http://localhost:4001/galleries/${id}`,
+      headers: JSON.parse(localStorage.getItem('user'))
+    }).then((response) => {
+      setGallery(response.data)
+    })
+  }, [id])
+
   return (
     <div>
-      
+      {gallery.name}
+      {gallery.content}
+      {gallery.location}
+      {gallery.tel}
+      {gallery.open_time}
+      {gallery.holiday}
+      {gallery.url}
     </div>
   )
 }
