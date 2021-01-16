9ecb09bf90053191640087291f489f7ea411ea99
watanabehideki <hideki.art.2280@gmail.com>
2021-01-16 12:00:20 +0900

    exhibitionsページ作成

--- src/components/pages/Exhibitions.js
+++ src/components/pages/Exhibitions.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import './Exhibitions.css'
 
 export default function Exhibitions() {
 
@@ -18,15 +19,39 @@ export default function Exhibitions() {
 
   return (
     <>
-      <h1>Exhibitions</h1>
-      {exhibitions.map(item => (
-        <Link to={`/exhibitions/${item.id}`}>
-          <li className='exceptions-wrap'> 
-            <h2>{item.title}</h2>      
-             <p>{item.gallery.name}</p>
-          </li>
-        </Link>
-      ))}
+      <div className='exhibitions-item-wrap' >
+        <li className='exhibitions-item'> 
+          {exhibitions.map(item => (
+            <Link  className='exhibitions-item-link' to={`/exhibitions/${item.id}`}>
+              <figure className='exhibitions-item-img'>
+                <img src={`./images/exhibition-${item.id}.jpg`} className='fade-img'/>
+              </figure>
+              <div className='exhibitions-item-info'>
+                <div className='exhibitions-item-title'>{item.title}</div>
+                <div className='exhibitions-item-name'>{item.gallery.name}</div>
+                <div className='exhibitions-item-date'>{item.start_date}-{item.end_date}</div>
+              </div>
+            </Link>
+          ))}
+        </li>
+      </div>
     </>
   )
 }
+
+{/* <>
+<li className='cards__item'>
+  <Link className='cards__item__link' to={props.path}>
+    <figure className='cards__item__pic-wrap' data-category={props.label}>
+      <img
+        className='cards__item__img'
+        alt='Travel Image'
+        src={props.src}
+      />
+    </figure>
+    <div className='cards__item__info'>
+      <h5 className='cards__item__text'>{props.text}</h5>
+    </div>
+  </Link>
+</li>
+</> */}
